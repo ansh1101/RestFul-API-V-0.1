ab44983b8c683423e0c921f295498dbd6407d912
Ansh Singh <anshi10.singh@gmail.com>
2021-11-26 10:46:49 +0530

    secnd

--- restfulapi.js
+++ restfulapi.js
@@ -15,15 +15,197 @@ const validation = require('./validation');
 const database = require('./dataBase.json')
 const mongoDB = true;
 const cors = require('cors');
+app.use(cors());
+
+
+
+// Database CRUD Operations Endpoint  //
+
+const { MongoClient } = require('mongodb');
+
+
+const { MongoClient } = require('mongodb');
+app.post('/DB/Create', (request, response) => {
+  
+
+async function main() {
+    const uri = 'mongodb+srv://adminadmin:adminadmin@studentsdatabase.l0wmf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        await listDatabases(client);
+        await createListing(client, request.body);
+
+
+    } catch (e) {
+        console.log(e);
+
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error)
+
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function createListing(client, newListing) {
+    const result = await client.db("studentDB").collection("students").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+response.status(201).send({ Response: "Data was successfully created" });
+});
+// Database Setup with Mongoose //
+
+mongoose.connect("mongodb://localhost:27017/studentdetailsDB");
+
+const studentSchema = new mongoose.Schema({
+    _id: Number,
+    name: String,
+    age: Number,
+    phone: Number,
+    address: String
+})
+
+const Student = mongoose.model("Student", studentSchema);
+
+// CRUD Operations with HTTP Calls and Local Database Depending upon whether Mongo is connected or Not //
+
+//    Create     //
+
+app.post('/DB/Create', (request, response) => {
+    if (mongoDB) {
+        Student.insertMany([request.body], function (err) {
+            if (err) {
+                response.status(400).send(err)
+            } else {
+                response.status(201).send({ Response: "Data was successfully created" });
+            }
+        });
+    }
+    else {
+        const body = request.body
+        database.push(body);
+        const successMessage = { Response: 'Data was successfully created' }
+        response.status(200).send(successMessage);
+    }
+
+});
+
+// To Read Particular Data//
+
+app.get('/DB/Read/:id', function (request, response) {
+    if (mongoDB) {
+        const recievedID = request.params.id
+        Student.find({ _id: recievedID }, function (err, student) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                response.status(200).send(student);
+
+                console.log(student)
+            }
+        });
+    } else {
+        response.status(200).send(database);
+
+    }
+
+});
+
+//      Update      //
+
+
+app.put('/DB/Update/:id', (request, response) => {
+    const body = request.body
+
+    if (mongoDB) {
+        Student.findByIdAndUpdate({ _id: request.params.id },
+            { $set: { name: body.name, age: body.age, phone: body.phone, address: body.phone } })
+            .then(result => {
+                response.status(200).send({ Data: "Data Updated Successfuly" })
+            }).catch(error => {
+                response.status(400).send(error);
+            });
+    } else {
+        let id = request.params.id;
+        let dbObtained = database.filter((DB) => {
+            return DB._id == id;
+        });
+        dbObtained[0].name = body.name;
+        dbObtained[0].age = body.age;
+        dbObtained[0].phone = body.phone;
+        dbObtained[0].address = body.address;
+        response.status(200).send({ Data: 'Data Updated Successfully' });
+    }
+});
+
+//      Delete      //
+
+app.delete('/DB/Delete', (request, response) => {
+    if (mongoDB) {
+        const id = request.query.id;
+
+        Student.findOneAndDelete({ _id: id }, (error) => {
+            const successMessage = { message: "Data Sucessfully Deleted" };
+            if (!error) {
+                response.status(200).send(successMessage)
+            }
+            else {
+                response.status(400).send(error)
+            }
+        });
+    } else {
+        const id = request.query.id;
+        let deleteDB = database.filter((deleteOne) => {
+            return deleteOne._id == id;
+        });
+        database.pop(deleteDB);
+        response.status(200).send({ data: 'data deleted successfully' })
+    }
+});
+
+
+//                                                     //Currency Converter Endpoint//
+
+// //ok Tested//
+
+app.post('/currencyConverter', (request, res) => {
+    let baseCurrency = request.body.baseCurrency
+    let goingToChangeCurrency = request.body.currency;
+    let amountRecieved = request.body.amount;
+
+    axios.get(`https://api.fastforex.io/fetch-multi?from=${goingToChangeCurrency}&to=${baseCurrency}&api_key=${process.env.API_KEY}`)
+        .then((response) => {
+
+            let convertedCurrency = response.data.results;
+            convertedCurrency[baseCurrency] = response.data.results[baseCurrency] * amountRecieved
+            let badRequest = 'Converting currency can not be same as Base currency'
+
+            if (goingToChangeCurrency === baseCurrency) {
+                res.status(400).send({ badRequest })
+            } else
+                res.status(200).send(convertedCurrency);
+        })
+
+        .catch((err) => {
+            console.error(err)
+        })
+});
+
+
 //Calculator Endpoint//
 
 // Additon and Subtraction with POST method
-
-app.use(cors());
-
-// const corsOptions = {
-//     origin = 'http://localhost:3001'
-// }
 
 app.post('/calculator/add', (request, response) => {
     let number1 = request.body.number1;
@@ -85,167 +267,6 @@ app.get('/calculator/division', (request, response) => {
         response.status(400).send({
             Error: "Invalid Number"
         })
-    }
-});
-
-
-//                                                     //Currency Converter Endpoint//
-
-// //ok Tested//
-
-app.post('/currencyConverter', (request, res) => {
-    let baseCurrency = request.body.baseCurrency
-    let goingToChangeCurrency = request.body.currency;
-    let amountRecieved = request.body.amount;
-
-    axios.get(`https://api.fastforex.io/fetch-multi?from=${goingToChangeCurrency}&to=${baseCurrency}&api_key=${process.env.API_KEY}`)
-        .then((response) => {
-
-            let convertedCurrency = response.data.results;
-            convertedCurrency[baseCurrency] = response.data.results[baseCurrency] * amountRecieved
-            let badRequest = 'Converting currency can not be same as Base currency'
-
-            if (goingToChangeCurrency === baseCurrency) {
-                res.status(400).send({ badRequest })
-            } else
-                res.status(200).send(convertedCurrency);
-        })
-
-        .catch((err) => {
-            console.error(err)
-        })
-});
-
-// Database CRUD Operations Endpoint  //
-
-// Database Setup with Mongoose //
-
-mongoose.connect("mongodb://localhost:27017/studentdetailsDB");
-
-const studentSchema = new mongoose.Schema({
-    _id: Number,
-    name: String,
-    age: Number,
-    phone: Number,
-    address: String
-})
-
-const Student = mongoose.model("Student", studentSchema);
-
-
-// CRUD Operations with HTTP Calls and Local Database Depending upon whether Mongo is connected or Not //
-
-
-//    Create     //
-
-
-app.post('/DB/Create', (request, response) => {
-    if (mongoDB) {
-        Student.insertMany([request.body], function (err) {
-            if (err) {
-                response.status(400).send(err)
-            } else {
-                response.status(201).send({ Response: "Data was successfully created" });
-            }
-        });
-    }
-    else {
-        const body = request.body
-        database.push(body);
-        const successMessage = { Response: 'Data was successfully created' }
-        response.status(200).send(successMessage);
-    }
-
-});
-
-// doubt //
-
-// const id =request.body._id;
-// let dbObtained = database.filter((DB) => {
-//     return DB._id == id;
-// });
-// if (dbObtained[0]._id == id) {
-//     response.status(400).send({ Error: "Dublication Of Data is Not Allowed" })
-// } else {
-//     database.push({
-//         _id: body.id,
-//         name: body.name,
-//         age: body.age,
-//         phone: body.phone,
-//         address: body.address
-//     });
-
-// To Read Whole Data//
-
-app.get('/DB/Read/:id', function (request, response) {
-    if (mongoDB) {
-        const recievedID = request.params.id
-        Student.find({ _id: recievedID}, function (err, student) {
-            if (err){
-                console.log(err);
-            }
-            else{
-                response.status(200).send(student  );
-                
-                console.log(student)
-            }
-        });
-    } else {
-        response.status(200).send(database);
-
-    }
-
-});
-
-//      Update      //
-
-
-app.put('/DB/Update/:id', (request, response) => {
-    const body = request.body
-
-    if (mongoDB) {
-        Student.findByIdAndUpdate({ _id: request.params.id },
-            { $set: { name: body.name, age: body.age, phone: body.phone, address: body.phone } })
-            .then(result => {
-                response.status(200).send({ Data: "Data Updated Successfuly" })
-            }).catch(error => {
-                response.status(400).send(error);
-            });
-    } else {
-        let id = request.params.id;
-        let dbObtained = database.filter((DB) => {
-            return DB._id == id;
-        });
-        dbObtained[0].name = body.name;
-        dbObtained[0].age = body.age;
-        dbObtained[0].phone = body.phone;
-        dbObtained[0].address = body.address;
-        response.status(200).send({ Data: 'Data Updated Successfully' });
-    }
-});
-
-//      Delete      //
-
-app.delete('/DB/Delete', (request, response) => {
-    if (mongoDB) {
-        const id = request.query.id;
-
-        Student.findOneAndDelete({ _id: id }, (error) => {
-            const successMessage = { message: "Data Sucessfully Deleted" };
-            if (!error) {
-                response.status(200).send(successMessage)
-            }
-            else {
-                response.status(400).send(error)
-            }
-        });
-    } else {
-        const id = request.query.id;
-        let deleteDB = database.filter((deleteOne) => {
-            return deleteOne._id == id;
-        });
-        database.pop(deleteDB);
-        response.status(200).send({ data: 'data deleted successfully' })
     }
 });
 
